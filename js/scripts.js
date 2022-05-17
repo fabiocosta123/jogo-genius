61deadf66c86d2cf415930fc90e48c64f594c113
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-05-17 00:23:36 -0300

    colocando < -- > no counter e movendo botao de ligar e desligar ao clicar

--- js/scripts.js
+++ js/scripts.js
@@ -1,88 +1,72 @@
 const _data = {
-	gameOn: false,
-	timeout: undefined,
-	sounds: [],
+  gameOn: false,
+  timeout: undefined,
+  sounds: [],
 
-	strict: false,
-	playerCanPlay: false,
-	score: 0,
-	gameSequence: [],
-	playerSequence: []
+  strict: false,
+  playerCanPlay: false,
+  score: 0,
+  gameSequence: [],
+  playerSequence: [],
 };
 
 const _gui = {
-	counter: document.querySelector(".gui__counter"),
-	switch: document.querySelector(".gui__btn-switch"),
-	led: document.querySelector(".gui__led"),
-	strict: document.querySelector(".gui__btn--strict"),
-	start: document.querySelector(".gui__btn--start"),
-	pads: document.querySelectorAll(".game__pad")
-}
+  counter: document.querySelector(".gui_counter"),
+  switch: document.querySelector(".gui_btn--switch"),
+  led: document.querySelector(".gui_led"),
+  strict: document.querySelector(".gui_btn--strict"),
+  start: document.querySelector(".gui_btn--start"),
+  pads: document.querySelectorAll(".game_pad"),
+};
 
 const _soundUrls = [
-	"audio/simonSound1.mp3",
-	"audio/simonSound2.mp3",
-	"audio/simonSound3.mp3",
-	"audio/simonSound4.mp3"
+  "audio/simonSound1.mp3",
+  "audio/simonSound2.mp3",
+  "audio/simonSound3.mp3",
+  "audio/simonSound4.mp3",
 ];
 
-_soundUrls.forEach(sndPath => {
-	const audio = new Audio(sndPath);
-	_data.sounds.push(audio);
+_soundUrls.forEach((sndPath) => {
+  const audio = new Audio(sndPath);
+  _data.sounds.push(audio);
 });
 
 _gui.switch.addEventListener("click", () => {
+  _data.gameOn = _gui.switch.classList.toggle("gui_btn--switch--on");
+  _gui.counter.classList.toggle("gui_counter--on");
+  _gui.counter.innerHTML = "--";
 
+  _data.strict = false;
+  _data.playerCanPlay = false;
+  _data.score = 0;
+  _data.gameSequence = [];
+  _data.playerSequence = [];
 });
 
-_gui.strict.addEventListener("click", () => {
+_gui.strict.addEventListener("click", () => {});
 
+_gui.start.addEventListener("click", () => {});
+
+const padListener = (e) => {};
+
+_gui.pads.forEach((pad) => {
+  pad.addEventListener("click", padListener);
 });
 
-_gui.start.addEventListener("click", () => {
+const startGame = () => {};
 
-});
+const setScore = () => {};
 
-const padListener = (e) => {
+const newColor = () => {};
 
-}
+const playSequence = () => {};
 
-_gui.pads.forEach(pad => {
-	pad.addEventListener("click", padListener);
-});
+const blink = (text, callback) => {};
 
-const startGame = () => {
+const waitForPlayerClick = () => {};
 
-}
+const resetOrPlayAgain = () => {};
 
-const setScore = () => {
+const changePadCursor = (cursorType) => {};
 
-}
-
-const newColor = () => {
-
-}
-
-const playSequence = () => {
-
-}
-
-const blink = (text, callback) => {
-
-}
-
-const waitForPlayerClick = () => {
-
-}
-
-const resetOrPlayAgain = () => {
-
-}
-
-const changePadCursor = (cursorType) => {
-
-}
-
-const disablePads = () => {
-
-}
+const disablePads = () => {};
